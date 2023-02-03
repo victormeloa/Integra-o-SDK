72c9856cdee24b8c40cae0d18a00978220653871
Victor Melo <e.s.m.victoraraujo@gmail.com>
2023-02-03 13:59:23 -0300

    Projeto 01.

---
+++ Projeto 01/src/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+int main()
+{
+
+int selection;
+
+std::cout << "escolha uma aplicação: " << std::endl;
+std::cout << "1. medidores de energia" << std::endl;
+std::cout << "2. medidores de água " << std::endl;
+std::cout << "3. religadores" << std::endl;
+std::cout << "4. sair da aplicação" << std::endl;
+
+while()
+
+
+if (selection==1)
+{
+    std::cout << "1. medidores de energia" << std::endl;
+}
+
+if (selection==2)
+
+{
+    std::cout << "2. medidores de agua" << std::endl;
+}
+
+if (selection==3)
+{
+    std::cout << "3. religadores" << std::endl;
+}
+
+if (selection==4)
+{
+    std::cout << "4. sair da aplicação" << std::endl;
+}
+
+
+return 0;
+} 
